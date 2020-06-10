769fab2aeaa372427d252a21731740a17e2703c8
afto95 <eric.pacheco@mail.utoronto.ca>
2020-06-10 19:59:02 -0400

    Added routes with data endpoints for each ethnicity

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from webscrape_ticker import num_2020_killings
 
@@ -23,17 +23,56 @@ class PoliceData(db.Model):
     armed = db.Column(db.String(80))
     
     def __repr__(self):
-        return f'{self.armed}, {self.raceethnicity}, {self.city}, {self.state}'
+        return f"({self.raceethnicity},{self.state},{self.latitude},{self.longitude},{self.armed})"
 
 #Create home route that returns index.html and allows javascript to access database data
 @app.route('/')
 def home():
-    # asian_data = db.query.filter_by(raceethnicity='A').all()
-    black_data = PoliceData.query.filter_by(raceethnicity='Black').all()
-    # white_data = db.query.filter_by(race='W').all()
-    # other_data = db.query.filter_by(race='O').all()
+    
     killings_count = num_2020_killings
     return render_template('index.html', **locals())
+
+
+#Create callable routes which can be accessed in javascript logic.js file. Add jsonify data to each endpoint
+@app.route('/blackData')
+def blackData():
+    data = {'results': []}
+    fields = ('race','state','latitude','longitude','armed')
+    black_data = PoliceData.query.filter_by(raceethnicity='Black').all()
+    for row in black_data:
+        data['results'].append({fields[0]:row.raceethnicity,fields[1]:row.state,fields[2]:row.latitude,fields[3]:row.longitude,fields[4]:row.armed,})
+    
+    return jsonify(data)
+
+@app.route('/whiteData')
+def whiteData():
+    data = {'results': []}
+    fields = ('race','state','latitude','longitude','armed')
+    white_data = PoliceData.query.filter_by(raceethnicity='White').all()
+    for row in white_data:
+        data['results'].append({fields[0]:row.raceethnicity,fields[1]:row.state,fields[2]:row.latitude,fields[3]:row.longitude,fields[4]:row.armed,})
+    
+    return jsonify(data)
+
+@app.route('/hispanicData')
+def hispanicData():
+    data = {'results': []}
+    fields = ('race','state','latitude','longitude','armed')
+    hispanic_data = PoliceData.query.filter_by(raceethnicity='Hispanic/Latino').all()
+    for row in hispanic_data:
+        data['results'].append({fields[0]:row.raceethnicity,fields[1]:row.state,fields[2]:row.latitude,fields[3]:row.longitude,fields[4]:row.armed,})
+    
+    return jsonify(data)
+
+@app.route('/asianData')
+def asianData():
+    data = {'results': []}
+    fields = ('race','state','latitude','longitude','armed')
+    asian_data = PoliceData.query.filter_by(raceethnicity='Asian/Pacific Islander').all()
+    for row in asian_data:
+        data['results'].append({fields[0]:row.raceethnicity,fields[1]:row.state,fields[2]:row.latitude,fields[3]:row.longitude,fields[4]:row.armed,})
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
