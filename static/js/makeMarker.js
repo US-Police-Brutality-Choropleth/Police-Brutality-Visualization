fbfe2ea11218f040542ca72d0383fbdc82b8f9b1
afto95 <eric.pacheco@mail.utoronto.ca>
2020-06-17 20:50:01 -0400

    'deploy2'

--- static/js/makeMarker.js
+++ static/js/makeMarker.js
@@ -1,14 +1,3 @@
-function makeMyMap(idCSS) {
-  var myMap = L.map(idCSS, {
-    center: [39.83, -98.58],
-    zoom: 4
-  });
-  return myMap
-}
-
-// Create map
-var myMap1 = makeMyMap("map1");
-
 // Adding tile layer to maps
 var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -17,26 +6,100 @@ var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/
   accessToken: API_KEY
 })
 
-tileLayer.addTo(myMap1)
+// Initialize all of the LayerGroups we'll be using
+var layers = {
+  White: new L.LayerGroup(),
+  Black: new L.LayerGroup(),
+  Asian: new L.LayerGroup(),
+  Hispanic: new L.LayerGroup()
+};
+
+//Create Map
+var myMap = L.map('map1', {
+  center: [39.83, -98.58],
+  zoom: 4,
+  layers: [
+    layers.White,
+    layers.Black,
+    layers.Asian,
+    layers.Hispanic
+  ]
+});
+
+tileLayer.addTo(myMap)
+
+// Create an overlays object to add to the layer control
+var overlays = {
+  "White": layers.White,
+  "Black": layers.Black,
+  "Asian": layers.Asian,
+  "Hispanic": layers.Hispanic
+};
+
+// Create a control for our layers, add our overlay layers to it
+L.control.layers(null, overlays).addTo(myMap);
+
+var icons = {
+  White: L.icon({
+    iconUrl:'static/Images/trump.png',
+    iconSize: [40,40]
+  }),
+  Black: L.icon({
+    iconUrl:'static/Images/dave.png',
+    iconSize: [80,80]
+  }),
+  Asian: L.icon({
+    iconUrl:'static/Images/jackie.png',
+    iconSize: [100,100]
+  }),
+  Hispanic: L.icon({
+    iconUrl:'static/Images/j_lo.png',
+    iconSize: [110,60]
+  })
+}
+
+//Function that adds markers to layer
+function createMarkers(layerGroup, raceIcon) {
+  for (var i=0;i<myResults.length;i++){
+    var location = myResults[i];
+    var lat=location.latitude;
+    var long=location.longitude;
+    var state=location.state;
+    var race= location.race;
+    var markers = L.marker([lat,long],{icon:raceIcon})
+    markers.addTo(layers[layerGroup])
+
+    markers.bindPopup(`<h1>State=${state}</h1>`+
+    `<h2>Race=${race}</h2>`)
+  }
+}
 
 // Function that loads data from flask for each race
-var races = ['White', 'Black', 'Hispanic', 'Asian',]
+var races = ['White', 'Black', 'Hispanic', 'Asian']
 races.forEach(race => {
   d3.json(`/2015killings/${race}`, data => {
     myResults = data.results 
-
-    for (var i=0;i<myResults.length;i++){
-      var location = myResults[i];
-      var lat=location.latitude;
-      var long=location.longitude;
-      var state=location.state;
-      var race= location.race;
-      L.marker([lat,long]).bindPopup(`<h1>Lat=${lat}</h1>`+
-      `<h2>Long=${long}</h2>`+
-      `<h3>State=${state}</h3>`+
-      `<h4>Race=${race}</h4>` +
-      `<img src="https://media.torchbox.com/original_images/blm.jpeg" height="25px" width="25px">`)
-      .addTo(myMap1)
-      }
+    var layerGroup;
+    var raceIcon;
+    if(race === 'White') {
+      layerGroup = 'White';
+      raceIcon = icons.White;
+      createMarkers(layerGroup, raceIcon)
+    }
+    else if(race === 'Black') {
+      layerGroup = 'Black';
+      raceIcon = icons.Black;
+      createMarkers(layerGroup, raceIcon)
+    }
+    else if(race === 'Asian') {
+      layerGroup = 'Asian';
+      raceIcon = icons.Asian;
+      createMarkers(layerGroup, raceIcon)
+    }
+    else {
+      layerGroup = 'Hispanic';
+      raceIcon = icons.Hispanic;
+      createMarkers(layerGroup, raceIcon)
+    }
   })
 });
