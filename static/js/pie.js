9429eccd3ddd00259614cb0887c86313776ca543
Afto <eric.pacheco@mail.utoronto.ca>
2020-06-18 15:11:17 -0400

    updated pie plots

--- static/js/pie.js
+++ static/js/pie.js
@@ -1,3 +1,28 @@
-function optionChanged(race){
-    
+function optionChanged(plot){
+  var plotImage = d3.select('.pie-plot')
+  var plotHeading = d3.select('.plot-title')
+  if (plot === 'Race Percentages') {
+    plotImage.html("<img src ='static/Images/pie_plots/race_pie.png'>")
+    plotHeading.text('Race Percentages Pie Chart')
   }
+  else if (plot === 'Fleeing') {
+    plotImage.html("<img src ='static/Images/pie_plots/flee_pie.png'>")
+    plotHeading.text('Fleeing Percentages Pie Chart')
+  }
+  else if (plot === 'Armed/Unarmed') {
+    plotImage.html("<img src ='static/Images/pie_plots/armed_unarmed_pie.png'>")
+    plotHeading.text('Armed or Unarmed Pie Chart')
+  }
+  else {
+    plotImage.html("<img src ='static/Images/pie_plots/year_pie.png'>")
+    plotHeading.text('Data Per Year Pie Chart')
+  }
+}
+
+var pie_plots = ['Race Percentages', 'Fleeing', 'Armed/Unarmed', 'Years']
+d3.select('#selDataset')
+  .selectAll('option')
+  .data(pie_plots)
+  .enter()
+  .append('option')
+  .text(plot => plot);
