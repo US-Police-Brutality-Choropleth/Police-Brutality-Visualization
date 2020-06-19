15a021598b0be5d76a355439a16cd524ef5a4e9a
Afto <eric.pacheco@mail.utoronto.ca>
2020-06-19 11:25:43 -0400

    Updated pie charts

--- static/js/pie.js
+++ static/js/pie.js
@@ -2,19 +2,19 @@ function optionChanged(plot){
   var plotImage = d3.select('.pie-plot')
   var plotHeading = d3.select('.plot-title')
   if (plot === 'Race Percentages') {
-    plotImage.html("<img src ='static/Images/pie_plots/race_pie.png'>")
+    plotImage.html("<img src ='static/Images/pie_plots/race_pie.png' width='700'>")
     plotHeading.text('Race Percentages Pie Chart')
   }
   else if (plot === 'Fleeing') {
-    plotImage.html("<img src ='static/Images/pie_plots/flee_pie.png'>")
+    plotImage.html("<img src ='static/Images/pie_plots/flee_pie.png' width='700'>")
     plotHeading.text('Fleeing Percentages Pie Chart')
   }
   else if (plot === 'Armed/Unarmed') {
-    plotImage.html("<img src ='static/Images/pie_plots/armed_unarmed_pie.png'>")
+    plotImage.html("<img src ='static/Images/pie_plots/armed_unarmed_pie.png' width='700'>")
     plotHeading.text('Armed or Unarmed Pie Chart')
   }
   else {
-    plotImage.html("<img src ='static/Images/pie_plots/year_pie.png'>")
+    plotImage.html("<img src ='static/Images/pie_plots/year_pie.png' width='700'>")
     plotHeading.text('Data Per Year Pie Chart')
   }
 }
